9e6ae18fb8d63dd983761394cda304308cda9d2b
Ingsam3 <leeye1021@naver.com>
2022-01-29 22:33:31 +0900

    회원 DTO개발

---
+++ src/main/java/com/example/springbootapi/model/MemberUpdateREquest.java
@@ -0,0 +1,16 @@
+package com.example.springbootapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder //인스턴스를 가독성 좋게 생성할 수 있는 이노테이션
+
+
+public class MemberUpdateREquest {
+    private  String name;
+}
