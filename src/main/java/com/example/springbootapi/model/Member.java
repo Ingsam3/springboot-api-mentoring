bd3b09b6a335d2bdc8a0db96cb0bde21379c7f12
Ingsam3 <leeye1021@naver.com>
2022-01-29 22:31:41 +0900

    member API

--- src/main/java/com/example/springbootapi/model/Member.java
+++ src/main/java/com/example/springbootapi/model/Member.java
@@ -1,4 +1,39 @@
 package com.example.springbootapi.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "member") /*테이블 생성*/
+@Builder
+
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    private  String name;
+
+    private  String email;
+
+    private  String number;
+
+    //MembercreateRequest => Member로 변환해주는 과정이 필요
+    public static Member toEntity(MemberCreateRequest request) {
+        return  Member.builder()
+                .name(request.getName())
+                .email(request.getEmail())
+                .number(request.getNumber())
+                .build();
+    }
+
+    public  void  updateName(String name){
+        this.name = name;
+    }
 }
